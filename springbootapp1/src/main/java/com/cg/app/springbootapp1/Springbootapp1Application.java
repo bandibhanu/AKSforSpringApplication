dd7538ccc9d420dbdaf8aeaded44e659b90288e2
bhanu bandi <bandibhanu97@outlook.com>
2019-06-26 06:01:38 +0000

    AKS

---
+++ springbootapp1/src/main/java/com/cg/app/springbootapp1/Springbootapp1Application.java
@@ -0,0 +1,14 @@
+package com.cg.app.springbootapp1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springbootapp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springbootapp1Application.class, args);
+		System.out.println("bhanu bandi");
+	}
+
+}
